61c752199dcc92024fa653b72d66d484fe4ff5ff
NelsonCruzG <nelsoncruz.1250@gmail.com>
2022-07-14 21:50:53 -0600

    Acept incoming changes

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 console.log("Hola mundo, primer console log");
-<<<<<<< HEAD
-console.log("hola desde la pc de: Jose");
-=======
+console.log("hola desde la PC de: Jose");
 console.log("Hola desde la PC de: Nelson");
->>>>>>> 25300cc0e03358c3e5b01ab5c19d12b43e5dfb57
